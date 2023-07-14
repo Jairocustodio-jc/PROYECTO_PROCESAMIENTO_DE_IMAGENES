8cf4cfcd8bab9c65f9245b4eb5012c71b9fa8283
Jairocustodio-jc <67087787+Jairocustodio-jc@users.noreply.github.com>
2023-07-14 05:16:22 -0500

    Add files via upload

--- YOLO_FINAL.py
+++ YOLO_FINAL.py
@@ -1,167 +1,219 @@
-# importar los paquetes necesarios
+import cv2
 import numpy as np
 import argparse
 import imutils
 import time
-import cv2
 import os
 
-# construir el analizador de argumentos y analizar los argumentos
+# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True,
-	help="ruta al video de entrada")
+    help="path to input video")
 ap.add_argument("-o", "--output", required=True,
-	help="ruta al video de salida")
+    help="path to output video")
 ap.add_argument("-y", "--yolo", required=True,
-	help="ruta base al directorio YOLO")
+    help="base path to YOLO directory")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="probabilidad mínima para filtrar detecciones débiles")
+    help="minimum probability to filter weak detections")
 ap.add_argument("-t", "--threshold", type=float, default=0.3,
-	help="umbral al aplicar supresión de no máxima")
+    help="threshold when applying non-maxima suppression")
 args = vars(ap.parse_args())
 
-# cargar las etiquetas de clase COCO en las que se entrenó el modelo YOLO
+# load the COCO class labels our YOLO model was trained on
 labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
 
-# inicializar una lista de colores para representar cada etiqueta de clase posible
+# initialize a list of colors to represent each possible class label
 np.random.seed(42)
 COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
 
-# derivar las rutas a los pesos y la configuración del modelo YOLO
-weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
-configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([args["yolo"], "yolov4.weights"])
+configPath = os.path.sep.join([args["yolo"], "yolov4.cfg"])
 
-# cargar el detector de objetos YOLO entrenado en el conjunto de datos COCO (80 clases)
-# y determinar solo los nombres de las capas *de salida* que necesitamos de YOLO
-print("[INFO] cargando YOLO desde el disco...")
+# load our YOLO object detector trained on COCO dataset (80 classes)
+# and determine only the *output* layer names that we need from YOLO
+print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
-ln = net.getLayerNames()
-ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+ln = net.getUnconnectedOutLayersNames()
 
-# inicializar el flujo de video, el puntero al archivo de video de salida y
-# las dimensiones del fotograma
+# initialize the video stream, pointer to output video file, and
+# frame dimensions
 vs = cv2.VideoCapture(args["input"])
 writer = None
 (W, H) = (None, None)
 
-# intentar determinar el número total de fotogramas en el archivo de video
-try:
-	prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
-		else cv2.CAP_PROP_FRAME_COUNT
-	total = int(vs.get(prop))
-	print("[INFO] {} fotogramas totales en el video".format(total))
+# Initialize variables for optical flow
+prev_gray = None
+prev_frame = None
+prev_pts = None
+lk_params = dict(winSize=(15, 15), maxLevel=2,
+                criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-# se produjo un error al intentar determinar el total
-# número de fotogramas en el archivo de video
-except:
-	print("[INFO] no se pudo determinar el número de fotogramas en el video")
-	print("[INFO] no se puede proporcionar un tiempo de finalización aproximado")
-	total = -1
+# Initialize variables for individual object tracking
+objects = {}
+object_id = 1
 
-# bucle sobre los fotogramas del flujo de video del archivo
+# Function to calculate optical flow and estimate velocity
+def estimate_velocity(frame, prev_frame, prev_gray, prev_pts, box):
+    if prev_gray is None or prev_pts is None or prev_pts.shape[0] == 0:
+        return 0.0
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Calculate optical flow using previous tracking points
+    next_pts, status, _ = cv2.calcOpticalFlowPyrLK(prev_gray, gray, prev_pts, None, **lk_params)
+
+    # Convert status to a boolean array
+    status = status.astype(np.bool)
+
+    # Get the coordinates of the object's center
+    x, y, w, h = box
+    center_x = x + w // 2
+    center_y = y + h // 2
+
+    # Filter valid tracking points around the object's center
+    valid_pts = next_pts[status[:, 0] & ((next_pts[:, 0, 0] >= center_x - w // 2) & (next_pts[:, 0, 0] <= center_x + w // 2) &
+                                         (next_pts[:, 0, 1] >= center_y - h // 2) & (next_pts[:, 0, 1] <= center_y + h // 2))]
+    valid_prev_pts = prev_pts[status[:, 0] & ((next_pts[:, 0, 0] >= center_x - w // 2) & (next_pts[:, 0, 0] <= center_x + w // 2) &
+                                               (next_pts[:, 0, 1] >= center_y - h // 2) & (next_pts[:, 0, 1] <= center_y + h // 2))]
+
+    # Check if there are enough valid points
+    if len(valid_pts) < 2 or len(valid_prev_pts) < 2:
+        return 0.0
+
+    # Calculate the average velocity
+    velocities = np.linalg.norm(valid_pts - valid_prev_pts, axis=1)
+    average_velocity = np.mean(velocities)
+
+    return average_velocity
+
+
+# loop over frames from the video file stream
 while True:
-	# leer el siguiente fotograma del archivo
-	(grabbed, frame) = vs.read()
+    # read the next frame from the file
+    (grabbed, frame) = vs.read()
 
-	# si el fotograma no se capturó, entonces hemos llegado al final
-	# del flujo
-	if not grabbed:
-		break
+    # if the frame was not grabbed, then we have reached the end
+    # of the stream
+    if not grabbed:
+        break
 
-	# si las dimensiones del fotograma están vacías, captúrelas
-	if W is None or H is None:
-		(H, W) = frame.shape[:2]
+    # if the frame dimensions are empty, grab them
+    if W is None or H is None:
+        (H, W) = frame.shape[:2]
 
-	# construir un blob a partir del fotograma de entrada y luego realizar un
-	# paso hacia adelante del detector de objetos YOLO, dándonos nuestras cajas delimitadoras
-	# y probabilidades asociadas
-	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-		swapRB=True, crop=False)
-	net.setInput(blob)
-	start = time.time()
-	layerOutputs = net.forward(ln)
-	end = time.time()
+    # construct a blob from the input frame and then perform a forward
+    # pass of the YOLO object detector, giving us our bounding boxes
+    # and associated probabilities
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+                                 swapRB=True, crop=False)
+    net.setInput(blob)
+    layerOutputs = net.forward(ln)
 
-	# inicializar nuestras listas de cajas delimitadoras detectadas, confidencias
-	# y IDs de clase, respectivamente
-	boxes = []
-	confidences = []
-	classIDs = []
+    # initialize our lists of detected bounding boxes, confidences,
+    # and class IDs, respectively
+    boxes = []
+    confidences = []
+    classIDs = []
 
-	# recorrer cada una de las salidas de capa
-	for output in layerOutputs:
-		# recorrer cada una de las detecciones
-		for detection in output:
-			# extraer la ID de clase y la confidencia (es decir, probabilidad)
-			# de la detección actual del objeto
-			scores = detection[5:]
-			classID = np.argmax(scores)
-			confidence = scores[classID]
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+        # loop over each of the detections
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability)
+            # of the current object detection
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
 
-			# filtrar las predicciones débiles asegurándose de que la detección
-			# probabilidad sea mayor que la probabilidad mínima
-			if confidence > args["confidence"]:
-				# escalar las coordenadas de la caja delimitadora de nuevo en relación a
-				# el tamaño de la imagen, teniendo en cuenta que YOLO
-				# realmente devuelve el centro (coordenadas x, y) de
-				# la caja delimitadora seguido del ancho y
-				# altura de las cajas
-				box = detection[0:4] * np.array([W, H, W, H])
-				(centerX, centerY, width, height) = box.astype("int")
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+            if confidence > args["confidence"]:
+                # scale the bounding box coordinates back relative to
+                # the size of the image, keeping in mind that YOLO
+                # actually returns the center (x, y)-coordinates of
+                # the bounding box followed by the boxes' width and
+                # height
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
 
-				# usar las coordenadas del centro (x, y) para derivar la esquina superior
-				# e izquierda de la caja delimitadora
-				x = int(centerX - (width / 2))
-				y = int(centerY - (height / 2))
+                # use the center (x, y)-coordinates to derive the top
+                # and and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
 
-				# actualizar nuestras listas de coordenadas de cajas delimitadoras,
-				# confidencias e IDs de clase
-				boxes.append([x, y, int(width), int(height)])
-				confidences.append(float(confidence))
-				classIDs.append(classID)
+                # update our list of bounding box coordinates,
+                # confidences, and class IDs
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
 
-	# aplicar supresión de no máxima para suprimir cajas delimitadoras débiles y superpuestas
-	idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
-		args["threshold"])
+    # apply non-maxima suppression to suppress weak, overlapping
+    # bounding boxes
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+                            args["threshold"])
 
-	# asegurarse de que al menos una detección exista
-	if len(idxs) > 0:
-		# recorrer los índices que estamos manteniendo
-		for i in idxs.flatten():
-			# extraer las coordenadas de la caja delimitadora
-			(x, y) = (boxes[i][0], boxes[i][1])
-			(w, h) = (boxes[i][2], boxes[i][3])
+    # ensure at least one detection exists
+    if len(idxs) > 0:
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
 
-			# dibujar un rectángulo de caja delimitadora y una etiqueta en el fotograma
-			color = [int(c) for c in COLORS[classIDs[i]]]
-			cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-			text = "{}: {:.4f}".format(LABELS[classIDs[i]],
-				confidences[i])
-			cv2.putText(frame, text, (x, y - 5),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            # draw a bounding box rectangle and label on the frame
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+            cv2.putText(frame, text, (x, y - 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-	# comprobar si el escritor de video es None
-	if writer is None:
-		# inicializar nuestro escritor de video
-		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-		writer = cv2.VideoWriter(args["output"], fourcc, 30,
-			(frame.shape[1], frame.shape[0]), True)
-		# alguna información sobre el procesamiento de un solo fotograma
-		if total > 0:
-			elap = (end - start)
-			print("[INFO] un solo fotograma tardó {:.4f} segundos".format(elap))
-			print("[INFO] tiempo total estimado para terminar: {:.4f}".format(
-				elap * total))
+            # Track individual objects and estimate velocity
+            object_box = (x, y, x + w, y + h)
+            if object_id not in objects:
+                objects[object_id] = {'box': object_box, 'velocities': [0.0], 'prev_pts': None}
+            else:
+                prev_pts = objects[object_id]['prev_pts']
+                velocity = estimate_velocity(frame, prev_frame, prev_gray, prev_pts, object_box)
+                objects[object_id]['velocities'].append(velocity)
 
-	cv2.imshow("Video", frame)
-	if cv2.waitKey(1) == ord('q'):
-		break
+            # Display object information with box and velocity
+            object_label = f"Object {object_id}"
+            object_velocity = np.mean(objects[object_id]['velocities'])
+            object_info = f"Velocity: {object_velocity:.2f}"
+            cv2.putText(frame, object_label, (x, y - 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            cv2.putText(frame, object_info, (x + int(w/2) - 50, y + int(h/2)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-	# escribir el fotograma de salida en disco
-	writer.write(frame)
+            # Update tracking points and object region for the next frame
+            objects[object_id]['prev_pts'] = cv2.goodFeaturesToTrack(prev_gray, maxCorners=100, qualityLevel=0.3,
+                                                                      minDistance=7, blockSize=7)
 
-# liberar los punteros de archivo
-print("[INFO] limpiando...")
-writer.release()
+            # Increment object ID for the next object
+            object_id += 1
+
+    # check if the video writer is None
+    if writer is None:
+        # initialize our video writer
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        writer = cv2.VideoWriter(args["output"], fourcc, 30,
+                                 (frame.shape[1], frame.shape[0]), True)
+
+    # write the output frame to disk
+    writer.write(frame)
+
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+    # Update previous frame and grayscale frame
+    prev_frame = frame.copy()
+    prev_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+# release the file pointers
 vs.release()
+writer.release()
+cv2.destroyAllWindows()
